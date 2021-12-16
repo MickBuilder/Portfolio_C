8360b4adae891e5a00f6b04aedb41d1ec70d0604
Mick Cool <monmail.mail>
2021-12-16 07:12:17 +0100

    Mise au propre du portfolio et continuation des TPs

---
+++ Competences.md
@@ -0,0 +1,18 @@
+# <u>Compétences à acquérir pour la programmation C.</u>
+
+| Compétences           | Points |     |     |
+|:---------------------:|:------:|:---:| --- |
+| Entrée/ Sorties       | 10     |     |     |
+| Types                 | 10     |     |     |
+| Programme             | 10     |     |     |
+| Modules               | 5      |     |     |
+| Compilation           | 10     |     |     |
+| Récursivité           | 4      |     |     |
+| Tableaux              | 8      |     |     |
+| Pointeurs             | 8      |     |     |
+| Structures            | 6      |     |     |
+| Allocation            | 4      |     |     |
+| Fichier               | 3      |     |     |
+| Bit à bit             | 2      |     |     |
+| Pointeur de fonctions | 2      |     |     |
+| Bibliothèques         | 5      |     |     |
---
+++ Travaux_Pratiques/TP4/Exo1/src/stack.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include "include/stack.h"
+
+/* This module proposes a single global and static stack */
+static Stack stack;
+
+/* Initialize correctly the stack */
+void init_stack(void)
+{
+    stack.size = MAX_SIZE;
+}
+
+/* Returns the current size of the stack. */
+int stack_size(void) {
+    return stack.size;
+}
+
+/* Returns 1 if the stack is empty, returns 0 otherwise. */
+int stack_is_empty(void) {
+    return stack.size == 0;
+}
+
+/* Returns the element at the top of the stack. */
+int stack_top(void) {
+    int size;
+    if (stack_is_empty()) {
+        return -1;
+    }
+    size = stack_size();
+    return stack.values[size - 1];
+}
+
+/* Pops the element at the top of the stack and returns it. */
+int stack_pop(void) {
+    int top;
+    if (stack_is_empty()) {
+        return -1;
+    }
+    top = stack_top();
+    stack.size--;
+    return top;
+}
+
+/* Pushes a given integer `n` at the top of the stack. */
+void stack_push(int n) {
+    if (stack_size() == MAX_SIZE) {
+        return ;
+    }
+    stack.values[stack_size()] = n;
+    stack.size++;
+}
+
+/* Displays the content of the stack on the standard output. */
+void stack_display(void) {
+    int i;
+
+    if (stack_is_empty()) {
+        printf("Stack is empty.\n");
+    }
+
+    printf("Values in the stack: ");
+    for (i = 0; i < stack_size(); i++) {
+        printf("%d ", stack.values[i]);
+    }
+    putchar('\n');
+}
+
+/* 
+ * Returns the element at index `index` inside the stack. The user is
+ * responsible of the use of this function. The answers may be not
+ * relevant if a call is done outside the current size of the
+ * stack. 
+*/
+int stack_get_element(int index) {
+    if (index >= stack_size()) {
+        return -1;
+    }
+    return stack.values[index];
+}
