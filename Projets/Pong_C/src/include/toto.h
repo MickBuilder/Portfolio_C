2489af1ab117736ff7ede015f6d65414d1fd1cc0
Mikdaam <mikdaam.badarou@edu.univ-eiffel.fr>
2022-01-06 11:51:53 +0100

    restart

--- Projets/Pong_C/src/include/toto.h
+++
@@ -1,6 +0,0 @@
-#ifndef _TOTO_H_
-#define _TOTO_H_
-
-int Add(int a, int b);
-
-#endif /* _TOTO_H_ */
--- Projets/Pong_C/src/toto.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "./include/toto.h"
-
-int Add(int a, int b)
-{
-    return a + b;
-}
